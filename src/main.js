5fe2d0e015f34b5d8c611ce74322dcac7824d881
SamuelGasasa <77382869+SamuelGasasa@users.noreply.github.com>
2021-01-29 12:56:37 +0200

    add button
    complete without tests

--- src/main.js
+++ src/main.js
@@ -2,12 +2,15 @@ const button=document.getElementById('add-button');
 button.addEventListener('click', Add);
 const ViewSection=document.getElementById('view-section');
 const input=document.getElementById('text-input');
-
 function Add(){
     let containerDiv=document.createElement('div');
+    containerDiv.classList.add('todo-container');
     let textDiv=document.createElement('dive');
+    textDiv.classList.add('todo-text');
     let dateDiv=document.createElement('div');
+    dateDiv.classList.add('todo-created-at');
     let priorityDiv=document.createElement('div');
+    priorityDiv.classList.add('todo-priority');
     let inputText=input.value;
     textDiv.innerText=inputText;
     let date=new Date();
@@ -17,5 +20,7 @@ function Add(){
     containerDiv.append(textDiv);
     containerDiv.append(dateDiv);
     containerDiv.append(priorityDiv);
+    input.value='';
+    input.focus();
     // alert();
 }
