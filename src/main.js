982ce7cd0159ff9954104a01c8b94cf9b8783346
SamuelGasasa <77382869+SamuelGasasa@users.noreply.github.com>
2021-01-30 15:48:22 +0200

    add in list ele

--- src/main.js
+++ src/main.js
@@ -3,12 +3,14 @@ button.addEventListener('click', Add);
 const ViewSection=document.getElementById('view-section');
 const input=document.getElementById('text-input');
 const counterDive=document.getElementById('counter');
-let nano=new Date();
-alert(nano.getSeconds());
+// let nano=new Date();
+// alert(nano.getSeconds());
+const list=document.getElementById('todoList');
 let counter=0;
 
 
 function Add(){
+    let li=document.createElement('li');
     let containerDiv=document.createElement('div');
     containerDiv.classList.add('todo-container');
     let textDiv=document.createElement('dive');
@@ -23,14 +25,16 @@ function Add(){
     dateDiv.innerText=createDate(date);
     priorityDiv.innerText=document.getElementById('priority-selector').value;
     ViewSection.append(containerDiv);
-    containerDiv.append(textDiv);
-    containerDiv.append(dateDiv);
     containerDiv.append(priorityDiv);
+    containerDiv.append(dateDiv);
+    containerDiv.append(textDiv);
     input.value='';
     input.focus();
     // alert();
     counter++;
     counterDive.innerText=counter;
+    li.append(containerDiv);
+    list.append(li);
 
 }
 
