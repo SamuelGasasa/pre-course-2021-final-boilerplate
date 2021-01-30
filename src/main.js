20d0811b36005d0f83c8c6c26c185de1cbbb87b1
SamuelGasasa <77382869+SamuelGasasa@users.noreply.github.com>
2021-01-30 17:30:37 +0200

    added sort button

--- src/main.js
+++ src/main.js
@@ -1,23 +1,24 @@
-const button=document.getElementById('add-button');
-button.addEventListener('click', Add);
+const addButton=document.getElementById('add-button');
+addButton.addEventListener('click', addToList);
 const ViewSection=document.getElementById('view-section');
 const input=document.getElementById('text-input');
-const counterDive=document.getElementById('counter');
+const counterDiv=document.getElementById('counter');
+const list=document.getElementById('todoList');
+const sortButton=document.getElementById('sort-button');
+sortButton.addEventListener('click',sortList)
 // let nano=new Date();
 // alert(nano.getSeconds());
-const list=document.getElementById('todoList');
 let counter=0;
 
-
-function Add(){
+function addToList(){
     let li=document.createElement('li');
+    let textDiv=document.createElement('div');;
     let containerDiv=document.createElement('div');
-    containerDiv.classList.add('todo-container');
-    let textDiv=document.createElement('dive');
-    textDiv.classList.add('todo-text');
     let dateDiv=document.createElement('div');
-    dateDiv.classList.add('todo-created-at');
     let priorityDiv=document.createElement('div');
+    containerDiv.classList.add('todo-container');
+    textDiv.classList.add('todo-text');
+    dateDiv.classList.add('todo-created-at');
     priorityDiv.classList.add('todo-priority');
     let inputText=input.value;
     textDiv.innerText=inputText;
@@ -32,7 +33,7 @@ function Add(){
     input.focus();
     // alert();
     counter++;
-    counterDive.innerText=counter;
+    counterDiv.innerText=counter;
     li.append(containerDiv);
     list.append(li);
 
@@ -43,4 +44,39 @@ function createDate(date) {
     let newDate;
     newDate=date.getFullYear()+'-'+(date.getMonth()+1)+'-'+date.getDate()+' '+ date.getHours()+':'+date.getMinutes()+':'+date.getSeconds();
     return newDate;
+}
+
+function sortList(){
+    let i, switching, b, shouldSwitch;
+    // list = document.getElementById("id01");
+    switching = true;
+    /* Make a loop that will continue until
+    no switching has been done: */
+    while (switching) {
+      // start by saying: no switching is done:
+      switching = false;
+      b = list.getElementsByClassName('todo-priority');
+      // Loop through all list-items:
+      for (i = 0; i < (b.length - 1); i++) {
+        // start by saying there should be no switching:
+        shouldSwitch = false;
+        /* check if the next item should
+        switch place with the current item: */
+        
+        if (Number(b[i].innerHTML) > Number(b[i + 1].innerHTML)) {
+          /* if next item is numerically
+          lower than current item, mark as a switch
+          and break the loop: */
+          shouldSwitch = true;
+        //   alert(b[i].closest('li').innerText);
+          break;
+        }
+      }
+      if (shouldSwitch) {
+        /* If a switch has been marked, make the switch
+        and mark the switch as done: */
+       list.insertBefore(b[i + 1].closest('li'), b[i].closest('li'));
+       switching = true;
+      }
+    }
 }
