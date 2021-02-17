090136cb3ab334545f9691a7ea2a2464698e3035
SamuelGasasa <77382869+SamuelGasasa@users.noreply.github.com>
2021-02-17 17:21:20 +0200

    post testing

--- backend/main.js
+++ backend/main.js
@@ -2,8 +2,21 @@ const express=require("express");
 const app=express();
 const fs=require("fs");
 
+app.use(express.json());
+
 app.get('/', (req,res)=>{
     res.send('<h1>Hello Samuel</h1>');
 });
+app.post('/post',(req,res)=>{
+    const {body}=req;
+    try{
+        fs.writeFileSync('./backend/storage/test.json',JSON.stringify(body));
+        res.send('successful post');
+    }
+    catch(e){
+        res.send(`error: ${e}`);
+    }
+    
+});
 
-app.listen(3000,() =>{console.log('listening on port 3000 successfully ')})
+app.listen(3000,() =>{console.log('listening on port 3000 successfully ')});
