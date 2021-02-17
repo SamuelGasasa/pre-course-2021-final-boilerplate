c404c13d5a0049d37d46b6a0bf86674097df2ca4
SamuelGasasa <77382869+SamuelGasasa@users.noreply.github.com>
2021-02-17 18:51:31 +0200

    backend

--- backend/main.js
+++ backend/main.js
@@ -1,22 +1,52 @@
 const express=require("express");
 const app=express();
 const fs=require("fs");
+const { get } = require("http");
+
 
 app.use(express.json());
 
-app.get('/', (req,res)=>{
-    res.send('<h1>Hello Samuel</h1>');
+app.get('/b', (req,res)=>{
+    const getAll=fs.readdirSync('./backend/storage');
+    console.log(getAll);
+    res.send(getAll);
 });
-app.post('/post',(req,res)=>{
+
+app.get('/b/:name',(req,res)=>{
+    const {name}=req.params;
+    try{
+        const file=fs.readFileSync(`./backend/storage/test${name}.json`);
+        const string=JSON.parse(file);
+        console.log(string);
+        res.send('found the file');
+    }
+    catch(e){
+        console.log('no such file');
+        res.send(`error: ${e}`);
+    }
+});
+
+app.post('/b',(req,res)=>{
     const {body}=req;
     try{
-        fs.writeFileSync('./backend/storage/test.json',JSON.stringify(body));
-        res.send('successful post');
+        const newFile=fs.writeFileSync(`./backend/storage/test${body.name}.json`,JSON.stringify(body));
+        res.send(newFile);
     }
     catch(e){
         res.send(`error: ${e}`);
     }
-    
+});
+
+app.put('/put',(req,res)=>{
+    let file=fs.readFileSync('./backend/storage/test.json');
+    let stringFile=JSON.parse(file);
+    console.log(stringFile.lastName);
+    res.send('hi');
+});
+
+app.delete('/delete',(req,res)=>{
+    fs.unlinkSync('./backend/storage/test.json');
+    res.send('file deleted');
 });
 
 app.listen(3000,() =>{console.log('listening on port 3000 successfully ')});
