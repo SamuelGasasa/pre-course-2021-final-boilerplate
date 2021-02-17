f7b4b1aadd1947839401ed15f3eb3832b77cfc51
SamuelGasasa <77382869+SamuelGasasa@users.noreply.github.com>
2021-02-17 13:53:06 +0200

    Revert "getid -test"
    This reverts commit b6e459972efc6ce7b01bd96d4a5f019aec550e04.

--- src/backend/script.js
+++ src/backend/script.js
@@ -1,27 +1,10 @@
 const express=require('express');
 const app=express();
 const {readFileSync} = require('fs');
-// app.use(express.json());
+app.use(express.json());
 
 app.get('/', (req, res) =>{
-    const data = readFileSync('./GET/get.json', 
-        {encoding:'utf8', flag:'r'});
-res.send(data);
+    res.send('<h1>Hello</h1>');
 });
-
-
-app.get('/:id', (req, res) =>{
-    const data = JSON.parse(readFileSync('./GET-ID/getID.json',
-        {encoding:'utf8', flag:'r'}));
-const id=req.params.id;
-for(let obj of data){
-    if(obj.id===id){ 
-        res.send(obj);
-    }
-}
-    res.send('error');
-
-});
-
 
 app.listen(3000);
